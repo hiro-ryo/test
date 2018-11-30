97bbd4116c894a0fd33f85aa1952bbaeeea166a7
hiro-ryo <hirose.ryo.0109@gmail.com>
2018-11-30 12:54:31 +0900

    commit

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -11,46 +11,6 @@ self.addEventListener('activate', function(e) {
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
 self.addEventListener('fetch', function(event) {});
 
-importScripts("https://www.gstatic.com/firebasejs/5.6.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/5.6.0/firebase-messaging.js");
-
-// firebaseCM
-var messaging = firebase.messaging();
-
-firebase.initializeApp({
-	messagingSenderId: "182384246777"
-});
-
-
-// プッシュ通知の受け取り
-self.addEventListener("push", function(event) {
-	//送られたプッシュ通知の本文を表示
-	if (Notification.permission == "granted") {
-		console.log("Push Notification Recieved", event);
-		event.waitUntil(
-			self.registration
-			.showNotification(event.data.json().notification.title, {
-				body: event.data.json().notification.body
-			})
-			.then(
-				function(showEvent) {},
-				function(error) {
-					console.log(error);
-				}
-			)
-		);
-	}
-});
-
-
-
-
-
-
-
-
-
-
 // 通知を受け取ったときの処理
 self.addEventListener("push", function(event) {
   console.log("Push Notification Recieved", event);
