7c91a6ca7201d704c9e20972c667b06d66195218
hiro-ryo <hirose.ryo.0109@gmail.com>
2018-12-03 10:03:36 +0900

    認証テスト

--- js/base.js
+++ js/base.js
@@ -10,14 +10,13 @@ window.onload = function(){
 				// サインイン成功時のコールバック関数
 				// 戻り値で自動的にリダイレクトするかどうかを指定
 				
-				document.getElementById('loader').style.display = 'block';
+				document.getElementById('main_content').style.display = 'block';
+				document.getElementById('firebaseui-auth-container').style.display = 'none';
 				
 				return true;
 			},
 			uiShown: function() {
 				// FirebaseUIウィジェット描画完了時のコールバック関数
-				// 読込中で表示しているローダー要素を消す
-				document.getElementById('loader').style.display = 'none';
 			}
 		},
 		 // リダイレクトではなく、ポップアップでサインインフローを表示
